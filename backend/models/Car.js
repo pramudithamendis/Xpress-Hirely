801c500e14cd3f105dba6a3aec74d21c8e1e32e9
Nimesha Abeyrathna <134591165+Nimesha07@users.noreply.github.com>
2024-04-21 10:21:48 +0530

    Adding files of Nimesha

---
+++ backend/models/Car.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create Schema
+const CarSchema = new Schema({
+    owner_Name: {
+        type: String,
+        required: true
+    },
+    owner_Email: {
+        type: String,
+        required: true
+    },
+    owner_ID: {
+        type: String,
+        required: true
+    },
+    VIN: {
+        type: String,
+        required: true
+    },
+    type: {
+        type: String,
+        required: true
+    },
+    brand: {
+        type: String,
+        required: true
+    },
+    model: {
+        type: String,
+        required: true
+    },
+    year: {
+        type: Number,
+        required: true
+    },
+    fuel_consumption: {
+        type: Number,
+        required: true
+    },
+    color: {
+        type: String,
+        required: true
+    },
+    features: {
+        type: [String],
+        required: true
+    },
+    price_per_km: {
+        type: String,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    status: {
+        type: String,
+        default: 'pending'
+    }
+});
+
+module.exports = Car = mongoose.model('Car', CarSchema);
