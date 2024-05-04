fb9e9347586f5427c37585728bb804be54007c9c
Nimesha Abeyrathna <134591165+Nimesha07@users.noreply.github.com>
2024-05-04 11:42:46 +0530

    Nimesha's missed carRoute.js addition

---
+++ backend/Routes/carRoute.js
@@ -0,0 +1,16 @@
+// routes/carRoutes.js
+const express = require('express');
+const router = express.Router();
+const carController = require('../controllers/carController');
+
+// Routes
+router.get('/', carController.getCars);
+router.post('/', carController.createCar);
+router.get('/:id', carController.getCarById);
+router.patch('/:id', carController.updateCar);
+router.delete('/:id', carController.deleteCar);
+router.get('/pending', carController.getPendingRequests);
+router.patch('/request/:id', carController.updateRequestStatus);
+router.delete('/request/:id', carController.deleteRequest);
+
+module.exports = router;
