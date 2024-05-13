01579a1765c68c5114be1edad331e66b8acbcad0
pramudithamendis <pramudithamendis444@gmail.com>
2024-05-13 15:07:07 +0530

    amended config.js

--- backend/config.js
+++ backend/config.js
@@ -1,7 +1,7 @@
 export const PORT = 5555;
 
-export const mongoDBURL = 'mongodb+srv://it22888884:alkeerwew@vehicle.7itxsn1.mongodb.net/vehicledb?retryWrites=true&w=majority'
+// export const mongoDBURL = 'mongodb+srv://it22888884:alkeerwew@vehicle.7itxsn1.mongodb.net/vehicledb?retryWrites=true&w=majority'
 
-export const SENDGRID_API_KEY = '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';
+// export const SENDGRID_API_KEY = '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';
 
-export const STRIPE_SECRET_KEY = 'sk_test_51P544cCOz1r3kP3kvY3jgdHlCaj4vSnWLZRjEd7ruP4rC6jvfQyvgPJ1j9wWxRJg5bgWJ0bCxXzseILvioJ2sPn000GWtkgJ3v';
+// export const STRIPE_SECRET_KEY = 'sk_test_51P544cCOz1r3kP3kvY3jgdHlCaj4vSnWLZRjEd7ruP4rC6jvfQyvgPJ1j9wWxRJg5bgWJ0bCxXzseILvioJ2sPn000GWtkgJ3v';
